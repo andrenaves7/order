90de0bfec9b9acf076adc4f94f02345fc2e3bb4a
andrenaves7 <andrenavesiii@gmail.com>
2021-02-15 12:52:37 -0300

    Creating API Spring Boot project

---
+++ api/src/main/java/com/github/andrenaves7/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.github.andrenaves7.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
